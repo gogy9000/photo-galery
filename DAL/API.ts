91ee7a5311f2a780ac4734b4d00441cd392017cf
gogy9000 <goropashnyj9000@gmail.com>
2022-10-02 20:34:35 +0500

    refactoring

--- DAL/API.ts
+++ DAL/API.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosResponse} from "axios";
-import {GetPhotosResponseEntityType, getPhotosParamsType} from "./types";
+import {GetPhotosResponseEntityType, getPhotosParamsType, GetPhotosItemReturnType} from "./types";
 
 
 
@@ -18,9 +18,11 @@ export const API = {
             {params: {per_page, page, order_by}})
             .then((response: AxiosResponse<GetPhotosResponseEntityType[]>) => {
                 console.log(response)
-                return response.data.map(item => ({
-                    imageUrls: item.urls.raw,
+                let returnedData:GetPhotosItemReturnType[]
+                returnedData=response.data.map(item => ({
+                    imageUrl: item.urls.raw,
                     author: item.user.username
                 }))
+                return returnedData
             })
 }
